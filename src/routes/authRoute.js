3f5524a28ba0e0195053a2220f184de091695ab2
utkarsh verma <77044451+vermautkarsh4488@users.noreply.github.com>
2024-11-29 17:25:57 +0530

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const { register, login, getProfile } = require('../controllers/authController');
+const { authenticate, authorize } = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', authenticate, getProfile);
+
+module.exports = router;
