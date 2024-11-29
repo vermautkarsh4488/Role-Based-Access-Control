71f6f8333d696e4eb838e591706b2aaab244b793
utkarsh verma <77044451+vermautkarsh4488@users.noreply.github.com>
2024-11-29 17:31:36 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Admin', 'Moderator', 'User'], default: 'User' },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
 
