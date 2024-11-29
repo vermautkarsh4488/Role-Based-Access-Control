65b75d0cc73913b47a3b8028df9c54286e65c8a0
utkarsh verma <77044451+vermautkarsh4488@users.noreply.github.com>
2024-11-29 17:27:02 +0530

    Update authValidator.js

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,1 +1,14 @@
+const Joi = require('joi');
 
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  password: Joi.string().min(8).required(),
+  role: Joi.string().valid('Admin', 'Moderator', 'User').default('User'),
+});
+
+const loginSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = { registerSchema, loginSchema };
